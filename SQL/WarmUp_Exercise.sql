3f996ed0f41cbed1e939b385411921c53dc3378e
Zhi Li <zli@babson.edu>
2017-09-19 20:01:53 -0400

    finished warm-up exercise.sql

--- SQL/WarmUp_Exercise.sql
+++ SQL/WarmUp_Exercise.sql
@@ -1,24 +1,26 @@
-USE AdventureWorks2012;
+use AdventureWorks2012;
 
-/*List info of all the sales order detail*/
+/*List info of all the sales order details*/
 SELECT *
 FROM Sales.SalesOrderDetail;
 
-/*Find out sales info about productID 843*/
+/*Find out sales info about Product (ID = 843, 845, 847)*/
 SELECT *
 FROM Sales.SalesOrderDetail
-WHERE ProductID = 843;
+WHERE productID in (843, 845, 847);
 
 /*Sales info of all the products of which unit price is between 100 and 200*/
 SELECT *
 FROM Sales.SalesOrderDetail
-WHERE UnitPrice >100 AND UnitPrice < 200;
+/*WHERE UnitPrice BETWEEN 100 AND 200*/
+WHERE UnitPrice >= 100 AND UnitPrice <= 200
+ORDER BY UnitPrice;
 
 /*All the store names*/
-SELECT name
+SELECT DISTINCT Name
 FROM Sales.Store;
 
 /*Find out store names that contain "Bike" */
 SELECT name
-FROM Sales.Store
-WHERE name LIKE '%Bike%';
+FROM sales.Store
+WHERE name LIKE ('%Bike%Shop%') OR name LIKE ('%Shop%Bike%');
