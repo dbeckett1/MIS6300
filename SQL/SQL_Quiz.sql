e934e0e54137a57c7bc4a3a752bebae9afb82a98
Zhi Li <lzblack@users.noreply.github.com>
2017-09-28 23:53:22 -0400

    added SQL Quiz

---
+++ SQL/SQL_Quiz.sql
@@ -0,0 +1,15 @@
+use AdventureWorks2012;
+
+/*a.	Show First name and last name of employees whose job title is “Sales Representative”, ranking from oldest to youngest. You may use HumanResources.Employee table and Person.Person table. (14 rows)*/
+
+
+
+/*b.	Find out all the products which sold more than $5000 in total. Show product ID and name and total amount collected after selling the products. You may use LineTotal from Sales.SalesOrderDetail table and Production.Product table. (254 rows)*/
+
+
+
+/*c.	Show BusinessEntityID, territory name and SalesYTD of all sales persons whose SalesYTD is greater than $500,000, regardless of whether they are assigned a territory. You may use Sales.SalesPerson table and Sales.SalesTerritory table. (16 rows)*/
+
+
+
+/*d.	Show the sales order ID of those orders in the year 2008 of which the total due is great than the average total due of all the orders of the same year. (3200 rows)*/
